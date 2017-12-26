2be46d112a3c41dc777c302b891e7703d9c3c5ac
Aris Santoso <32691152+yohanesaris44@users.noreply.github.com>
2017-12-26 19:15:28 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TUGAS_Teori_TCC</title>
+</head>
+<body>
+	<h1>Tugas Teori TCC Docker Compose</h1>
+	<h4><br>Nama: Aris Santoso</br></h4>
+	<h4>Nim		: 155410071</h4>
+	<h4>Git Hub : https://github.com/yohanesaris44</h4>
+	
+</body>
+</html>
